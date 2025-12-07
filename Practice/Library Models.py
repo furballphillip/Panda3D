a3040b06d872a3d95693da2232aaa821ec34478c
Phillip Boehm <136853335+furballphillip@users.noreply.github.com>
2025-12-06 18:01:23 -0600

    Node parenting changes
    Working on understanding nodes and NodePaths.

--- Practice/Library Models.py
+++ Practice/Library Models.py
@@ -2,24 +2,32 @@ from panda3d.core import loadPrcFile, Loader
 loadPrcFile("Configs/config.prc")
 
 from direct.showbase.ShowBase import ShowBase
+from panda3d.core import NodePath
 
 class GameWindow(ShowBase):
-    loader: Loader
+    loader: Loader # Explicit type definition for loader
     
     def __init__(self):
         super().__init__()
+
+        empty = NodePath("empty")
 
         env = self.loader.loadModel("models/environment")
         env.reparentTo(self.render)
 
         box = self.loader.loadModel("models/box")
-        box.setPos(0, 10, 0)
-        box.reparentTo(self.render)
+        box.setPos(0, 5, 0)
+        box.reparentTo(empty)
+        boxNode = box.node()
+        print(type(box))
+        print(type(boxNode))
 
         panda = self.loader.loadModel("models/panda")
-        panda.setPos(1, 15, 0)
+        panda.setPos(2, 0, 0)
         panda.setScale(0.3, 0.3, 0.3)
-        panda.reparentTo(self.render)
+        panda.reparentTo(box)
+
+        empty.reparentTo(self.render)
 
 game = GameWindow()
 game.run()
