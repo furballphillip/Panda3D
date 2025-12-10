6da165c347bd713774e3ee8d5fa2f6c03f45e775
Phillip Boehm <136853335+furballphillip@users.noreply.github.com>
2025-12-09 21:37:06 -0600

    Imported model with animations from blender into Panda3D
    To my surprise, it worked! After successfully importing the .glb, the model, textures, and animations all looked how they should!

---
+++ Practice_1/Animations.py
@@ -0,0 +1,46 @@
+from panda3d.core import loadPrcFile, Loader, AmbientLight, DirectionalLight
+loadPrcFile("Configs/config.prc")
+
+from direct.showbase.ShowBase import ShowBase
+from direct.actor.Actor import Actor
+
+filepath = "Character.glb" 
+
+class GameWindow(ShowBase):
+    loader: Loader # Explicit type definition for loader
+    
+    def __init__(self):
+        super().__init__()
+
+        # Load the model directly
+        character = self.loader.loadModel(filepath)
+        self.actor = Actor(character)
+        self.actor.reparentTo(self.render)
+        self.actor.setPos(0, 50, 3)
+        self.actor.setScale(2, 2, 2)
+
+        # Add lighting
+        aLight = AmbientLight("ambient")
+        aLight.setColor((0.5, 0.5, 0.5, 1))
+        aLightNP = self.render.attachNewNode(aLight)
+        self.render.setLight(aLightNP)
+
+        dLight = DirectionalLight("directional")
+        dLight.setColor((1, 1, 1, 1))
+        dLightNP = self.render.attachNewNode(dLight)
+        dLightNP.setHpr(45, -45, 0)
+        self.render.setLight(dLightNP)
+
+        # Show available animations tracks
+        anim_names = self.actor.getAnimNames()
+        print("Animations:", anim_names)
+
+        # play the first animation
+        if anim_names:
+            self.actor.loop("(Sitting) Walk")
+
+        env = self.loader.loadModel("models/environment")
+        env.reparentTo(self.render)
+
+game = GameWindow()
+game.run()
