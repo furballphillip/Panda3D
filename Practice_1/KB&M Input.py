5dcdd34e4e66b21e2089eb49095007aa8f7a1b54
Phillip Boehm <136853335+furballphillip@users.noreply.github.com>
2025-12-15 19:27:32 -0600

    Added mouse detection

--- Practice_1/KB&M Input.py
+++ Practice_1/KB&M Input.py
@@ -25,7 +25,19 @@ class GameWindow(ShowBase):
         self.jack.setHpr(0, 180, 180)
         self.jack.reparentTo(self.render)
 
-        # Key Binds
+        # disable default mouse controls
+        self.disable_mouse()
+
+        # Mouse binds
+        self.accept("mouse1", self.mouse_click)
+        self.accept("mouse1-up", self.mouse_click)
+        self.accept("mouse2", self.mouse_click)
+        self.accept("mouse2-up", self.mouse_click)
+        self.accept("mouse3", self.mouse_click)
+        self.accept("mouse3-up", self.mouse_click)
+
+
+        # Keyboard Binds
         self.accept("a", updateKeyMap, ["left", True])
         self.accept("a-up", updateKeyMap, ["left", False])
         self.accept("d", updateKeyMap, ["right", True])
@@ -43,10 +55,25 @@ class GameWindow(ShowBase):
 
         self.taskMgr.add(self.updateTask, "update")
 
+    def mouse_click(self): 
+        # mb = self.win.getPointer(0) 
+        # print(mb.getX(), mb1.getY()) # range determined by resolution
+
+        mouse_button = self.mouseWatcherNode.getMouse() # range is -1 to 1
+        print(mouse_button.getX(), mouse_button.getY())
+
     def updateTask(self, task):
         dt = globalClock.getDt() # type: ignore
 
         pos = self.jack.getPos()
+
+        # md = self.win.getPointer(0)
+        # print(mb.getX(), mb.getY()) # range determined by resolution
+
+        #if self.mouseWatcherNode.hasMouse(): # range is -1 to 1
+         #   x = self.mouseWatcherNode.getMouseX()
+          #  y = self.mouseWatcherNode.getMouseY()
+           # print(x,y)
         
         if keyMap["left"]:
             pos.x -= self.speed * dt
