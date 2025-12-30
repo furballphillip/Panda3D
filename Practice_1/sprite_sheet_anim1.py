923da0da8df74efa2deaef88b8fce2795c1391eb
Phillip Boehm <136853335+furballphillip@users.noreply.github.com>
2025-12-29 22:10:47 -0600

    Sprite sheet animation part 1
    I had trouble getting the sprite sheet to display on a plane, in both .glb and .egg format for some reason. Instead I just ended up making the plane in Panda3D using CardMaker.

---
+++ Practice_1/sprite_sheet_anim1.py
@@ -0,0 +1,33 @@
+from direct.showbase.ShowBase import ShowBase
+from panda3d.core import loadPrcFileData, CardMaker, TransparencyAttrib
+
+configVars = """
+win-size 1920 1080
+show-frame-rate-meter True
+"""
+
+loadPrcFileData("", configVars)
+
+class GameWindow(ShowBase):
+    def __init__(self):
+        super().__init__()
+
+        self.set_background_color(0.1, 0.1, 0.1, 1)
+
+        cm = CardMaker("sprite")
+        cm.setFrame(-1, 1, -1, 1)  # Creates a plane from (-1, -1) to (1, 1)
+
+        plane = render.attachNewNode(cm.generate())
+        plane.setTexture(loader.loadTexture("models/knight.png"))
+        plane.setTransparency(TransparencyAttrib.MAlpha)
+
+        self.taskMgr.add(self.updateManagers, "Update texture")
+
+    def update_texture(self, task):
+
+        return task.cont
+
+
+        
+game = GameWindow()
+game.run()
