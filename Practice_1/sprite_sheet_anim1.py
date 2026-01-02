564fdc987129bc13b6e23f54dd12781a4d3c691b
Phillip Boehm <136853335+furballphillip@users.noreply.github.com>
2026-01-02 00:53:20 -0600

    Fully added sprite animation
    Tried to see if ChatGPT or Copilot could solve the mystery of why my animation was way too fast and how to slow it down. Spoiler alert, they both utterly failed.
    
    Delta time was having some issues working, so I just adjusted the if statement in update_texture and it worked perfectly fine. Not sure how I feel about the animation being tied to the framerate, but for now it works.

--- Practice_1/sprite_sheet_anim1.py
+++ Practice_1/sprite_sheet_anim1.py
@@ -1,9 +1,10 @@
 from direct.showbase.ShowBase import ShowBase
-from panda3d.core import loadPrcFileData, CardMaker, TransparencyAttrib
+from panda3d.core import loadPrcFileData, CardMaker, TransparencyAttrib, TextureStage
 
 configVars = """
 win-size 1920 1080
-show-frame-rate-meter True
+show-frame-rate-meter True 
+window-title Sprite Sheet Animation Example
 """
 
 loadPrcFileData("", configVars)
@@ -15,9 +16,10 @@ class GameWindow(ShowBase):
         self.set_background_color(0.1, 0.1, 0.1, 1)
 
         cm = CardMaker("sprite")
-        cm.setFrame(-1, 1, -1, 1)  # Creates a plane from (-1, -1) to (1, 1)
+        cm.setFrame(-1, 1, -1, 1) # Creates a plane
+        cm.setUvRange((0.0, 0.0), (0.5, 1.0)) # First frame only cropping
         
-        plane = render.attachNewNode(cm.generate())
+        self.plane = render.attachNewNode(cm.generate())
 
         texture = loader.loadTexture("models/knight.png")
         
@@ -25,12 +27,22 @@ class GameWindow(ShowBase):
         texture.setMinfilter(texture.FTNearest)
         texture.setMagfilter(texture.FTNearest)
 
-        plane.setTexture(texture)
-        plane.setTransparency(TransparencyAttrib.MAlpha)
+        self.plane.setTexture(texture)
+        self.plane.setTransparency(TransparencyAttrib.MAlpha)
 
-        self.taskMgr.add(self.updateManagers, "Update texture")
+        self.tx = 0.0
+        self.tx_offset = 1 / 2
+        self.texture_update = 0
+
+        self.taskMgr.add(self.update_texture, "Update texture")
 
     def update_texture(self, task):
+        self.plane.setTexOffset(TextureStage.getDefault(), self.tx, 0)
+
+        self.texture_update += 1
+        if self.texture_update > 100: # Update animation every X frames
+            self.tx += self.tx_offset
+            self.texture_update = 0
 
         return task.cont
         
